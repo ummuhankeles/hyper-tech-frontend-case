fd258c0f519e9bbc5becb6cc9b95a4057691e0b5
ummuhankeles <ummuhankeles4@gmail.com>
2025-03-28 12:33:20 +0300

    product search added.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const API_URL = 'https://api.hyperteknoloji.com.tr/products/list';
 const API_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJMb2dpblR5cGUiOiIxIiwiQ3VzdG9tZXJJRCI6IjU1NzI0IiwiRmlyc3ROYW1lIjoiRGVtbyIsIkxhc3ROYW1lIjoiSHlwZXIiLCJFbWFpbCI6ImRlbW9AaHlwZXIuY29tIiwiQ3VzdG9tZXJUeXBlSUQiOiIzMiIsIklzUmVzZWxsZXIiOiIwIiwiSXNBUEkiOiIxIiwiUmVmZXJhbmNlSUQiOiIiLCJSZWdpc3RlckRhdGUiOiIzLzI1LzIwMjUgMTowMDo0OCBQTSIsImV4cCI6MjA1NDEzNDExMCwiaXNzIjoiaHR0cHM6Ly9oeXBlcnRla25vbG9qaS5jb20iLCJhdWQiOiJodHRwczovL2h5cGVydGVrbm9sb2ppLmNvbSJ9.i_pqc2C5vSh1IegikQTkSxYk6MsjALLzp4g30KXqunM';
 const API_KEY = 'de59806d4c0449f69f38a62c4e72874a';
 
+let productList = [];
+
 async function fetchData() {
     try {
         const response = await fetch(API_URL, {
@@ -19,7 +21,7 @@ async function fetchData() {
         }
 
         const result = await response.json();
-        const productList = result.data.slice(0, 12);
+        productList = result.data.slice(0, 12);
         getProductList(productList);
     } catch (error) {
         console.error(error);
@@ -32,20 +34,33 @@ function getProductList(productList) {
     loader.style.display = 'block';
 
     let items = '';
-    productList.forEach(element => {
-        items += `
-            <div class="card" style="width: 18rem;">
-                <img src="${element.productData.productMainImage}" class="card-img-top" alt="${element.productName}">
-                <div class="card-body">
-                    <h5 class="card-title">${element.productName}</h5>
-                    <p class="card-text">${element.marketPrice} TL</p>
-                    <button>Ürün Detay<i class="fa-solid fa-arrow-right ms-3"></i></button>
+    if (productList.length === 0) {
+        items = '<div class="not-found"><img class="mb-3" src="assets/not-found.png" alt="not-found"/><p>Aradığınız ürün bulunamadı.</p></div>';
+    } else {
+        productList.forEach(element => {
+            items += `
+                <div class="card" style="width: 18rem;">
+                    <img src="${element.productData.productMainImage}" class="card-img-top" alt="${element.productName}">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.productName}</h5>
+                        <p class="card-text">${element.marketPrice} TL</p>
+                        <button>Ürün Detay<i class="fa-solid fa-arrow-right ms-3"></i></button>
+                    </div>
                 </div>
-            </div>
-        `
-    });
+            `
+        });
+    }
     productCard.innerHTML = items;
     loader.style.display = 'none';
+}
+
+function searchProducts() {
+    const query = document.getElementById('searchInput').value.toLowerCase();
+    const filteredProducts = productList.filter(product => {
+        return product.productName.toLowerCase().includes(query);
+    });
+    
+    getProductList(filteredProducts);
 }
 
 fetchData();
